a28307689bdfe8d7c46157c2775877067f03ec0f
Diego Diaz <valthalion@gmail.com>
2020-12-20 00:30:37 +0100

    Day 19 Part 2

--- puzzle19.py
+++ puzzle19.py
@@ -1,9 +1,10 @@
+from itertools import product
 import re
 
 
 def read_input():
     ruleset = {}
-    with open('puzzle19.in.test2', 'r') as f:
+    with open('puzzle19.in', 'r') as f:
         while (line := next(f).strip()):
             rule_no, rule_spec = line.split(': ')
             ruleset[int(rule_no)] = Rule.from_spec(rule_spec, ruleset)
@@ -13,7 +14,7 @@ def read_input():
 
 class Rule:
     def __init__(self, spec, ruleset):
-        self._regex = None
+        self._patterns = None
         self._spec = spec
         self._ruleset = ruleset
 
@@ -28,40 +29,74 @@ class Rule:
         rule_nos = [[int(n) for n in group.strip().split()] for group in groups]
         return AlternativeRule(rule_nos, ruleset)
 
-    def regex(self, bounds=True):
-        if self._regex is None:
-            self._regex = self._eval_regex()
-        if bounds:
-            return f'^{self._regex}$'
-        return self._regex
+    @property
+    def patterns(self):
+        if self._patterns is None:
+            self._patterns = self._eval_patterns()
+        return self._patterns
 
-    def _eval_regex(self):
+    def _eval_patterns(self):
         raise NotImplementedError
+
+    def match(self, txt):
+        return txt in self.patterns
 
 
 class LeafRule(Rule):
-    def _eval_regex(self):
-        return self._spec
+    def _eval_patterns(self):
+        return {self._spec}
 
 
 class SequenceRule(Rule):
-    def _eval_regex(self):
-        return ''.join(self._ruleset[rule].regex(bounds=False) for rule in self._spec)
+    def _eval_patterns(self):
+        combinations = product(*(self._ruleset[rule].patterns for rule in self._spec))
+        return set(''.join(patterns) for patterns in combinations)
 
 
 class AlternativeRule(Rule):
-    def _eval_regex(self):
-        regexstr = '|'.join(
-            ''.join(self._ruleset[rule].regex(bounds=False) for rule in group)
-            for group in self._spec)
-        return f'({regexstr})'
+    def _eval_patterns(self):
+        patterns = set()
+        for group in self._spec:
+            combinations = product(*(self._ruleset[rule].patterns for rule in group))
+            patterns |= set(''.join(pattern_pair) for pattern_pair in combinations)
+        return patterns
+
+
+def match(ruleset, msg):
+    rule42, rule31 = ruleset[42].patterns, ruleset[31].patterns
+    sample = rule42.pop()
+    chunksize = len(sample)
+    rule42.add(sample)
+    n = len(msg)
+    if n % chunksize != 0:
+        return False
+    chunks = tuple(msg[idx : idx+chunksize] for idx in range(0, n, chunksize))
+    n_chunks = len(chunks)
+
+    start42s = 0
+    for chunk in chunks:
+        if chunk in rule42:
+            start42s += 1
+        else:
+            break
+
+    if not 1 <= start42s < n_chunks:
+        return False
+    if start42s < n_chunks - start42s + 1:
+        return False
+
+    for chunk in chunks[start42s:]:
+        if chunk not in rule31:
+            return False
+    return True
 
 
 def part_1():
     ruleset, messages = read_input()
-    regex = re.compile(ruleset[0].regex())
-    return sum(1 for msg in messages if regex.match(msg))
+    rule = ruleset[0]
+    return sum(1 for msg in messages if rule.match(msg))
 
 
 def part_2():
-    pass
+    ruleset, messages = read_input()
+    return sum(1 for msg in messages if match(ruleset, msg))
